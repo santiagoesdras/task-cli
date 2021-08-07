457e1f49da5fc999bd72bcd57dab0d0b5c281698
KenethSandoval <kenethra.74@gmail.com>
2021-08-06 21:37:53 -0600

    feat: add menu

--- task-cli.py
+++ task-cli.py
@@ -1,15 +1,26 @@
-dbTasks = []
+dbTasks = ["test", "learning python"]
 
 def addTask(task):
-  dbTask.append(task)
+  dbTasks.append(task)
 
 def listTask():
   for task in dbTasks:
-    print(task)
+    print("* ", task)
 
-taskInput = input("Title for new task: ")
+def removeTask(i):
+  dbTasks.pop(i)
 
-addTask(taskInput)
+def menu(option):
+  if option == "1":
+    taskInput = input("Title for new task: ")
+    addTask(taskInput)
 
-listTask()
+  elif option == "2":
+    listTask()
+
+print("Options for you task: \n 1. Add new Task \n 2. List tasks")
+
+menuOption = input("Ingrese una opcion ")
+
+menu(menuOption)
 
