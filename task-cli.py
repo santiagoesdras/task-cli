6fd986478974f4018fe0ef64971a4bb18b8f2125
KenethSandoval <kenethra.74@gmail.com>
2021-08-06 22:52:23 -0600

    feat: delete tasks

--- task-cli.py
+++ task-cli.py
@@ -1,18 +1,11 @@
 dbTasks = ["test", "learning python"]
 
-def booleanvalue(ciclo):
-  if ciclo > 0:
-    return 1
-  elif ciclo <=0:
-    return 0
-
 # add new task in array
 def addTask(task):
   dbTasks.append(task)
 
 # list the tasks in the array
 def listTask():
-  dbTasks.reverse()
   for task in dbTasks:
     print("* ", task)
 
@@ -29,44 +22,40 @@ def menu(option):
     quit()
 
   elif option == "1":
-    taskInput = input("Title for new task: ")
+    taskInput = input("\n\tTitle for new task: ")
     addTask(taskInput)
-    ciclo = +1
-    booleanvalue(ciclo)
-
+    
   elif option == "2":
     listTask()
-    ciclo = +1
-    booleanvalue(ciclo)
-
+   
   elif option == "3":
     try:
-      removeInput = input("What task do you want to delete?: ")
+      removeInput = input("\n\tWhat task do you want to delete?: ")
       removeTask(int(removeInput))
     except:
-      print("Not a number")
+      print("\tNot a number")
   
   elif option == "4":
     try:
-      editInput = input("What task do you want to update?: ")
-      updateInput = input("What title will you put it?: ")
+      editInput = input("\n\tWhat task do you want to update?: ")
+      updateInput = input("\n\tWhat title will you put it?: ")
       editTask(int(editInput), updateInput)
     except: 
-      print("Not a number")
+      print("\tNot a number")
 
   elif option == "5":
-    print("Total tasks: ", len(dbTasks))
+    print("\n\tTotal tasks: ", len(dbTasks))
 
   else:
-    print("Option not valid")
+    print("\tOption not valid")
 
 
 def imenu():
-    print("\n\tOptions for you task: \n\t0.Exit \n\t1.Add new Task \n\t2.List tasks \n\t3.Delete tasks")
+    print("\n\tOptions for you task: \n\t0.Exit \n\t1.Add new Task \n\t2.List tasks \n\t3.Delete tasks \n\t4.Update tasks \n\t5.Total tasks")
 
     menuOption = input("\tEnter an option ")
     menu(menuOption)
 
-while(booleanvalue):
+while(1):
   imenu()
  
